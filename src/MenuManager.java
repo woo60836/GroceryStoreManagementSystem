6f30ee695fe8f3726e99767e7f0a1ae9995d0c9a
woo60836 <woo60836@naver.com>
2023-03-14 21:16:12 +0900

    removed hello.java and added menumanager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,9 +1,12 @@
 
-public class hello {
+public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		
+		System.out.println("1. Add Grocery");
+		System.out.println("2. Delete Grocery");
+		System.out.println("3. Edit Grocery");
+		System.out.println("4. View Grocery");
+		System.out.println("5. Exit");
 	}
 
 }
