194f26a66ead9f8e80785e753a034398ebeef756
woo60836 <woo60836@naver.com>
2023-04-11 11:48:00 +0900

    change double to int

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		int num =  0;
 		Scanner input = new Scanner(System.in);
-
+		GroceryManager grocerymanager = new GroceryManager(input);
+		
+		int num =  0;
 		while (num != 6) {
 			System.out.println("*** GroceryStore Management System Menu ***");
 			System.out.println("1. Add Grocery"); // food type, food name, price, origin, prduct no.
@@ -17,16 +18,16 @@ public class MenuManager {
 			System.out.print("Select one number between 1-6 : ");
 			num = input.nextInt(); 
 			if (num == 1) {
-				AddGrocery();
+				grocerymanager.AddGrocery();
 			}
 			else if (num == 2) {
-				DeleteGrocery();
+				grocerymanager.DeleteGrocery();
 			}
 			else if (num == 3) {
-				EditGrocery();
+				grocerymanager.EditGrocery();
 			}
 			else if (num == 4) {
-				ViewGrocery();
+				grocerymanager.ViewGrocery();
 			}
 			else {
 				continue;
@@ -35,39 +36,4 @@ public class MenuManager {
 
 	}
 	
-	public static void AddGrocery() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Food Type : ");
-		String FoodType = input.next();
-		
-		System.out.print("Food Name : ");
-		String FoodName = input.next();
-		
-		System.out.print("Price : ");
-		int Price = input.nextInt();
-		
-		System.out.print("Country of Origin : ");
-		String ProductOrigin = input.next();
-		
-		System.out.print("Product No. : ");
-		int ProductNo = input.nextInt();
-	}
-	
-	public static void DeleteGrocery() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Product No. : ");
-		int ProductNo = input.nextInt();
-	}
-
-	public static void EditGrocery() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Product No. : ");
-		int ProductNo = input.nextInt();
-	}
-	
-	public static void ViewGrocery() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Product No. : ");
-		int ProductNo = input.nextInt();
-	}
 }
